36d79417606e883601097c9c5a1237c17b9f5413
Aisyatush Shofiah <aisyatush_shofiah_24rpl@student.smktelkom-mlg.sch.id>
2017-08-13 01:12:11 +0700

    TMED.05-AddMediaNotification - Aisyatush Shofiah

--- app/src/main/java/com/example/android/classicalmusicquiz/QuizActivity.java
+++ app/src/main/java/com/example/android/classicalmusicquiz/QuizActivity.java
@@ -30,6 +30,7 @@ import android.support.v4.media.session.MediaButtonReceiver;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
